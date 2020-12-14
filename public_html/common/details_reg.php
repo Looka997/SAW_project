24f740e8396fe0ac86278a41710eef5eadb0673a
Looka997 <luca.marinelli997@gmail.com>
2020-12-14 16:11:24 +0100

    updates regex details, min len array and max len array

--- public_html/common/details_reg.php
+++ public_html/common/details_reg.php
@@ -1,16 +1,16 @@
 <?php 
-    $fstname_reg = '/.+/';
-    $lastname_reg = '/.+/';
+    $fstname_reg = '/^[^\s].+/';
+    $lastname_reg = '/^[^\s].+/';
     $email_reg = '/.+@.+\.[a-zA-Z]+/';
     $pass_reg = '/.+/';
-    $username_reg= '/.+/';
+    $username_reg= '/^[^\s].+/';
     $address_reg = '/(^$|.+)/';
     $phone_reg = '/(^$|.+)/';
-    $design_name_reg = '/.+/';
+    $design_name_reg = '/^[^\s].+/';
     $design_price_reg = '/[1-9][0-9]*([\.,][0-9][1-9]*)?/';
 
-    $min_len = array("firstname" => 1, "lastname" => 1, "email" => 5, "pass" => 1);
-    $max_len = array("firstname" => 255, "lastname" => 255, "email" => 254, "pass" => 255);
+    $min_len = array("firstname" => 1, "lastname" => 1, "email" => 5, "pass" => 1, "username" => 1, "address" => 1, "phone" => 1, "design_name" => 1, "design_price" => 1);
+    $max_len = array("firstname" => 255, "lastname" => 255, "email" => 254, "pass" => 255, "username" => 256, "address" => 256, "phone" => 15, "design_name" => 256, "design_price" => 13);
 
     function is_valid_length($str, $minlen, $maxlen){
         $strlen = strlen($str);
