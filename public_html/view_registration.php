1d9e1ab148c7516868e9ab4419fa8c3cee37e418
Looka997 <luca.marinelli997@gmail.com>
2020-12-14 16:25:02 +0100

    marks what information is mandatory

--- public_html/view_registration.php
+++ public_html/view_registration.php
@@ -14,20 +14,24 @@
         require("common/navbar.php")
     ?>
 
-
+<section>
     <form action="registration.php" method="POST" id="registerForm">
-            <label for="firstname">First name:</label>
-            <input type="text" id="firstname" name="firstname">
-            <label for="lastname">Last name:</label>
-            <input type="text" id="lastname" name="lastname">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email">
-            <label for="pass">Password:</label>
-            <input type="password" id="pass" name="pass">
-            <label for="confirm">Confirm password:</label>
-            <input type="password" id="confirm" name="confirm">
-            <input type="submit" name="submit" value="submit">
+                <label for="firstname">*First name:</label>
+                <input type="text" id="firstname" name="firstname">
+                <label for="lastname">*Last name:</label>
+                <input type="text" id="lastname" name="lastname">
+                <label for="email">*Email</label>
+                <input type="email" id="email" name="email">
+                <label for="pass">*Password:</label>
+                <input type="password" id="pass" name="pass">
+                <label for="confirm">*Confirm password:</label>
+                <input type="password" id="confirm" name="confirm">
+                <input type="submit" name="submit" value="submit">
     </form>
+    <p>Fields marked by * are mandatory</p>
+</section>
+    
+
 
     <?php require_once("common/footer.php"); ?>
 
