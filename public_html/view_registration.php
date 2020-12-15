c3289d462f35e96ca1ea2fc30f1cd321c2714ffc
WolfenCLI <WolfenCLI@zohomail.eu>
2020-12-15 10:33:08 +0100

    adds regex to registration form

--- public_html/view_registration.php
+++ public_html/view_registration.php
@@ -11,7 +11,8 @@
 <body>
     <?php 
         session_start();
-        require("common/navbar.php")
+        require("common/navbar.php");
+        require_once("common/details_reg.php");
     ?>
 <?php 
     if (isset($_SESSION["registration_POST"])){
@@ -21,21 +22,54 @@
 <section>
     <form action="registration.php" method="POST" id="registerForm">
                 <label for="firstname">*First name:</label>
-                <input type="text" id="firstname" name="firstname" value="<?php if (isset($restoring) && isset($restoring['firstname'])) echo $restoring["firstname"] ?>">
+                <input
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    value="<?php if (isset($restoring) && isset($restoring['firstname'])) echo $restoring["firstname"] ?>"
+                    pattern="<?php echo preg_replace("/\//", "", $fstname_reg) ?>">
+
                 <label for="lastname">*Last name:</label>
-                <input type="text" id="lastname" name="lastname" value="<?php if (isset($restoring) && isset($restoring['lastname'])) echo $restoring["lastname"] ?>">
+                <input
+                    type="text"
+                    id="lastname"
+                    name="lastname"
+                    value="<?php if (isset($restoring) && isset($restoring['lastname'])) echo $restoring["lastname"] ?>"
+                    pattern="<?php echo preg_replace("/\//", "", $lastname_reg) ?>">
+                
                 <label for="email">*Email</label>
                 <input type="email" id="email" name="email" value="<?php if (isset($restoring) && isset($restoring['email'])) echo $restoring["email"] ?>">
+
                 <label for="pass">*Password:</label>
                 <input type="password" id="pass" name="pass">
+
                 <label for="confirm">*Confirm password:</label>
                 <input type="password" id="confirm" name="confirm">
+
                 <label for="username">Username:</label>
-                <input type="text" id="username" name="username" value="<?php if (isset($restoring) && isset($restoring['username'])) echo $restoring["username"] ?>">
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value="<?php if (isset($restoring) && isset($restoring['username'])) echo $restoring["username"] ?>"
+                    pattern="<?php echo preg_replace("/\//", "", $username_reg) ?>">
+
                 <label for="address">Address:</label>
-                <input type="text" id="address" name="address" value="<?php if (isset($restoring) && isset($restoring['address'])) echo $restoring["address"] ?>">
+                <input
+                    type="text"
+                    id="address"
+                    name="address"
+                    value="<?php if (isset($restoring) && isset($restoring['address'])) echo $restoring["address"] ?>"
+                    pattern="<?php echo preg_replace("/\//", "", $address_reg) ?>">
+
                 <label for="phone">Phone:</label>
-                <input type="text" id="phone" name="phone" value="<?php if (isset($restoring) && isset($restoring['phone'])) echo $restoring["phone"] ?>">
+                <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    value="<?php if (isset($restoring) && isset($restoring['phone'])) echo $restoring["phone"] ?>"
+                    pattern="<?php echo preg_replace("/\//", "", $phone_reg) ?>">
+
                 <input type="submit" name="submit" value="submit">
     </form>
     <p>Fields marked by * are mandatory</p>
