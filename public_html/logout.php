3455d2b3e6720ae3f8dc361971d7f346522bd103
Maerch96 <alessiomorciano1996@gmail.com>
2020-12-19 18:40:11 +0100

    fixes logout redirection

--- public_html/logout.php
+++ public_html/logout.php
@@ -4,7 +4,7 @@
     setcookie(session_name(), "", 0);
     session_unset();
     session_destroy();
-    header("Location: ../view_login.php");
+    header("Location: view_login.php");
     exit;
 ?>
 
