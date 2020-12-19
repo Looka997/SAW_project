a9e4c9612bc28f1ebd26a2f6a5787acc8b087b1e
Maerch96 <alessiomorciano1996@gmail.com>
2020-12-19 18:34:39 +0100

    rebase da master

--- public_html/common/navbar.php
+++ public_html/common/navbar.php
@@ -6,28 +6,44 @@
     }
 ?>
 
-<nav class="navbar">
-    <ul>
-        <?php if (isset($_SESSION["email"])){
+<!--    <li> <a href="allusers.php">All users list</a></li> -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Inserisci nome del sito qui</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <?php if (isset($_SESSION["email"])){
             if (isset($_SESSION["admin"]) && $_SESSION["admin"] )
-                echo "<li><a href=\"../db_init/db_init.php\">DB RESET</a></li>";
-            echo "<li> Hi " . htmlspecialchars(view_name()) . " </li>";
+                echo "<li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"../db_init/db_init.php\">DB RESET</a>
+                </li>";
+            echo "<li class=\"nav-item\">  
+            <a class=\"nav-link\" href=\"show_profile.php\"> Hi " . htmlspecialchars(view_name()) ." </a>".
+            "</li>";
         } ?>
-        <li><a href="view_create.php"> Design your own </a></li>
-        <li><a href="view_designs.php">Designs by the community</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="view_create.php"> Design your own</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="view_designs.php">Designs by the community</a>
+        </li>
         <?php 
         if (!isset($_SESSION["email"])){
-            echo "<li><a href=\"view_login.php\">Accedi</a></li>";
-            echo "<li><a href=\"view_registration.php\">Registrati</a></li>";
+            echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"view_login.php\">Accedi</a></li>";
+            echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"view_registration.php\">Registrati</a></li>";
         }
         else{
-            echo "<li><a href=\"logout.php\">Logout</a></li>";
-            echo "<li><a href=\"show_profile.php\">Profilo</a></li>";
-            echo "<li><a id='cart_btn' href='cart.php'>Carrello</a></li>";
+            echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"common/logout.php\">Logout</a></li>";
+            echo "<li class=\"nav-item\"><a class=\"nav-link\" id='cart_btn' href='cart.php'>Carrello</a></li>";
         }
         ?>
-        <li> <a href="allusers.php">All users list</a></li>
-    </ul>
+      </ul>
+    </div>
+  </div>
 </nav>
 
 <!-- Cart counter JS -->
