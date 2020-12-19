582a66a3bf5794ed42dd4cd8d9245b3f01b87c2a
WolfenCLI <WolfenCLI@zohomail.eu>
2020-12-19 18:13:49 +0100

    blocks common and classes and redirects for 404

---
+++ public_html/not-found.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page not found</title>
+</head>
+<body>
+    <h1>The requested page could not be found</h1>
+    <img src="https://i.imgur.com/xwfI7Hw.png" alt="mmmmh, error 404">
+</body>
+</html>
