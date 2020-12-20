8cdef7c3467654b40335b87fd38beb66f0d8880b
Federico Crippa <WolfenCLI@zohomail.eu>
2020-12-20 18:50:50 +0100

    fixes old code remaining after merge conflict

--- public_html/index.php
+++ public_html/index.php
@@ -8,15 +8,6 @@
   <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-  <?php
-  session_start();
-  if (!isset($_SESSION["email"])) {
-    header("Location: view_login.php");
-    exit;
-  }
-  require("common/navbar.php");
-  ?>
-
   <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
     <div id="index-centered" class="col-md-5 p-lg-5 mx-auto my-5">
       <h1 class="light-txt display-4 fw-normal">Crea il tuo stile!</h1>
