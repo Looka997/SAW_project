dd610bd17a2d65c999e8eac1edfdef89b175cd9c
WolfenCLI <WolfenCLI@zohomail.eu>
2020-12-20 14:06:38 +0100

    implements logout emptying cart

--- public_html/logout.php
+++ public_html/logout.php
@@ -4,7 +4,19 @@
     setcookie(session_name(), "", 0);
     session_unset();
     session_destroy();
-    header("Location: view_login.php");
-    exit;
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+</head>
+<body>
+    <script src="js/cart.js"></script>
+    <script>
+        cartEmpty();
+    </script>
+    <meta http-equiv="refresh" content="0.1; url=index.php">
+</body>
+</html>
